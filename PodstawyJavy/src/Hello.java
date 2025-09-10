368385379929b68637fca3ab0a5e02d300816b0e
kacperkordaszewski2006@gmail.com <jakub3szczesniak@gmail.com>
2025-09-10 11:30:51 +0200

    Stworzenie projektu

---
+++ PodstawyJavy/src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+       // System.out.println("Hello World");
+        System.out.println("Kacper Kordaszewski");
+        System.out.println("5cI");
+    }
+}
