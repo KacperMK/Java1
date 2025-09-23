c8e2a8e7c1d1524bf9a500f80a2013534055c91d
KacperMK <kacperkordaszewski2006@gmail.com>
2025-09-23 09:01:04 +0200

    zad1

--- JavaZadanie1/src/Main.java
+++ JavaZadanie1/src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        double stopnieC, stopnieF;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj stopnnie Celsjusza: ");
+        stopnieC=scanner.nextDouble();
+        stopnieF=1.8 * stopnieC + 32;
+        System.out.println("Stopnie f:" + stopnieF);
     }
 }
