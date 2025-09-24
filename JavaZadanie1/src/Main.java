3cc568cdb8861cd8d9cd3fd056cb8e1822fb3887
kacperkordaszewski2006@gmail.com <jakub3szczesniak@gmail.com>
2025-09-24 11:10:03 +0200

    zadanie 1,2,3

--- JavaZadanie1/src/Main.java
+++ JavaZadanie1/src/Main.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+
+//        ZADANIE 1
+
         double stopnieC, stopnieF;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj stopnnie Celsjusza: ");
@@ -11,6 +15,9 @@ public class Main {
         stopnieF=1.8 * stopnieC + 32;
         System.out.println("Stopnie f:" + stopnieF);
 
+
+
+//        ZADANIE 2
 
         double z1, z2, z3;
         Scanner scanner1 = new Scanner(System.in);
@@ -20,10 +27,44 @@ public class Main {
         z2=scanner1.nextDouble();
         System.out.println("Trzecia liczba");
         z3=scanner1.nextDouble();
-        if (z1>z2){
-            System.out.println(z1+" jest najwieksze");
-        }else{
-            System.out.println(z2 + "jest najwieksze");
+        if (z1>z2) {
+            if (z3>z1) {
+                System.out.println(z3 + "jest najwieksze");
+            }else{
+                System.out.println(z1 + " jest najwieksze");
+            }
+        } else if (z2>z3) {
+            if (z2 > z1){
+                System.out.println(z2 + " jest najwieksze");
+            }else{
+                System.out.println(z1 + " jest najwieksze");
+            }
+        } else if (z3>z1) {
+            if (z2>z3){
+                System.out.println(z1 + " jest najwieksze");
+            }else{
+                System.out.println(z3 + " jest najwieksze");
+            }
+        }
+
+
+
+//        ZADANIE 3
+
+        double waga, wzrost, bmi;
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Podaj wage:");
+        waga= scanner3.nextDouble();
+        System.out.println("Podaj wzrost:");
+        wzrost= scanner3.nextDouble();
+        wzrost = wzrost/100;
+        bmi = waga / (wzrost * wzrost);
+        if (bmi < 18.5){
+            System.out.println("Niedowaga");
+        } else if (bmi > 24.9) {
+            System.out.println("Nadwaga");
+        } else{
+            System.out.println("Waga prawidlowa");
         }
     }
 }
